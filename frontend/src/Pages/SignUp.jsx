2bd79189c34122574ca01861c2a364ddf8fd22ad
hazecodez <muhammedhashir298@gmail.com>
2024-03-23 15:43:07 +0530

    Login and Signup UI

--- frontend/src/Pages/SignUp.jsx
+++ frontend/src/Pages/SignUp.jsx
@@ -1,11 +1,86 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  Card,
+  Input,
+  Button,
+  Typography,
+} from "@material-tailwind/react";
+import {useNavigate} from 'react-router-dom'
 
 function SignUp() {
+  const navigate = useNavigate();
+
+  const [name,setName] = useState('');
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('')
   return (
-    <div>
-      
+    <div className="w-full flex justify-center items-center h-screen">
+      <Card color="transparent" shadow={false}>
+      <Typography variant="h4" color="blue-gray">
+        Register your Account
+      </Typography>
+      <Typography color="gray" className="mt-1 font-normal">
+        Nice to meet you! Enter your details to register.
+      </Typography>
+      <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+        <div className="mb-1 flex flex-col gap-6">
+          <Typography variant="h6" color="blue-gray" className="-mb-3">
+            Your Name
+          </Typography>
+          <Input
+          value={name}
+          onChange={(e)=> setName(e.target.value)}
+            size="lg"
+            placeholder="name@mail.com"
+            className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+            labelProps={{
+              className: "before:content-none after:content-none",
+            }}
+          />
+          <Typography variant="h6" color="blue-gray" className="-mb-3">
+            Your Email
+          </Typography>
+          <Input
+          value={email}
+          onChange={(e)=> setEmail(e.target.value)}
+            size="lg"
+            placeholder="name@mail.com"
+            className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+            labelProps={{
+              className: "before:content-none after:content-none",
+            }}
+          />
+          <Typography variant="h6" color="blue-gray" className="-mb-3">
+            Password
+          </Typography>
+          <Input
+          value={password}
+          onChange={(e)=> setPassword(e.target.value)}
+            type="password"
+            size="lg"
+            placeholder="********"
+            className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+            labelProps={{
+              className: "before:content-none after:content-none",
+            }}
+          />
+        </div>
+        
+        <Button className="mt-6" fullWidth>
+          Register
+        </Button>
+        <Typography color="gray" className="mt-4 text-center font-normal">
+          Already have an account?{" "}
+          <a onClick={()=>{
+            navigate('/login')
+          }} className="font-medium text-gray-900">
+            Log In
+          </a>
+        </Typography>
+      </form>
+    </Card>
     </div>
-  )
+  );
 }
 
-export default SignUp
+export default SignUp;
--- frontend/src/Routes/UserRoutes.jsx
+++ frontend/src/Routes/UserRoutes.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import {Route,Routes} from 'react-router-dom'
+import Home from '../Pages/Home'
+import Login from '../Pages/Login'
+import SignUp from '../Pages/SignUp'
 
 export default function UserRoutes() {
   return (
     <div>
-      
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/signup' element={<SignUp/>} />
+      </Routes>
     </div>
   )
 }
